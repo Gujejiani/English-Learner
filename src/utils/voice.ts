70d58134da5239f64dacff6fb44f716acdd4d6d9
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2023-08-28 21:33:05 +0400

    weird sound issue fixed for mobile

--- src/utils/voice.ts
+++ src/utils/voice.ts
@@ -1,11 +1,17 @@
+export const speak = (msg: string, geo?: boolean): void => {
+  const sp = new SpeechSynthesisUtterance(msg);
+  sp.rate = 0.8;
 
-  export const speak = (msg: string, geo?: boolean): void => {
-    const sp = new SpeechSynthesisUtterance(msg);
-    sp.rate = 0.8;
-    if(geo){
-      sp.lang ='ka-GE'
-    }
-    [sp.voice] = speechSynthesis.getVoices();
-    speechSynthesis.speak(sp);
-    
+  // Get the list of available voices
+  const availableVoices = speechSynthesis.getVoices();
+  console.log(availableVoices);
+
+  // for mobile setting 146
+  if (window.innerWidth <= 768 ) {
+    sp.voice = availableVoices[146];
+  } else {
+    [sp.voice] = availableVoices;
   }
+
+  speechSynthesis.speak(sp);
+};
