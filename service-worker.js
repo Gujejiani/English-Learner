71ba83046178a145e84aeb8223d2f4dda6121c81
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-05-05 20:55:23 +0400

    Updates

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// Define a cache name
+const CACHE_NAME = 'your-app-cache';
+
+// List of assets to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/js/main.js',
+  // Add more URLs of your app's assets here
+];
+
+// Install service worker and cache assets
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached assets when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
